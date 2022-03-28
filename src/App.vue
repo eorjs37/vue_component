27dd40ae1840a1c0bc57d62f35123b3a5dac5a39
maxgunchoi <maxgunchoi@carrotglobal.com>
2022-03-29 08:54:31 +0900

    vue table 적용

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="mw-1200">
+    <h1>
+      Table Component
+    </h1>
+    <Table :head="tableHead" :body="tableBody"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Table from '@/components/Table.vue';
+import { ref } from 'vue';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  setup(){
+    const tableHead = ref(['company','Contact','Country']);
+    const tableBody = ref(
+      [
+        {item1:'Alfreds Futterkiste', item2:'	Maria Anders', item3:'	Germany'},
+        {item1:'Centro comercial Moctezuma', item2:'Francisco Chang', item3:'Mexico'},
+        {item1:'Ernst Handel', item2:'Roland Mendel', item3:'Austria'},
+        {item1:'Island Trading', item2:'Helen Bennett', item3:'UK'},
+        {item1:'Laughing Bacchus Winecellars', item2:'Yoshi Tannamuri', item3:'Canada'},
+      ]
+    );
+    return{
+      tableHead,
+      tableBody
+    }
+  },
+  components:{
+    Table
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  margin: 0;
+}
+
+h1,h2,h3,h4,h5,h6{
+  margin: 0;
+}
+
+.mw-1200{
+  max-width: 1200px;
+  width: 100%;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/Table.vue
@@ -0,0 +1,61 @@
+<template>
+  <table>
+    <thead>
+        <tr>
+            <th v-for="(item,index) in head" :key="'head'+index">
+                {{item}}
+            </th>
+        </tr>
+    </thead>
+   <tbody v-if="body.length > 0">
+       <tr v-for="(item, index) in body" :key="'body'+index">
+           <td>{{ item.item1 }}</td>
+           <td>{{ item.item2 }}</td>
+           <td>{{ item.item3 }}</td>
+       </tr>
+   </tbody>
+   <tbody v-else>
+       <tr>
+           <th colspan="99">데이터가 존재하지 않습니다.</th>
+       </tr>
+   </tbody>
+ </table>
+</template>
+<script>
+export default {
+    props:{
+        head:{
+            type: Array,
+            required: true,
+            default(){
+                return []
+            }
+        },
+        body:{
+            type: Array,
+            required: true,
+            default(){
+                return []
+            }
+        }
+    },
+}
+</script>
+
+<style scoped>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
